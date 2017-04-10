fec34e8a1a465b2a8446b2e5cb76745aad987df5
lpicon <lpicon@bancodebogota.com.co>
2017-04-10 10:28:09 -0500

    cambios en circleci.yml21

--- generate_artifact.sh
+++ generate_artifact.sh
@@ -1,5 +1,5 @@
 timestamp=`date +%Y%m%d%H%M%S`
 branch=`git branch | grep \* | cut -d ' ' -f2`
-war_file=`ls target/*.jar | head -n 1`
+jar_file=`ls target/*.jar | head -n 1`
 
 curl -X PUT "https://bancodebogota.jfrog.io/bancodebogota/repojuli/${timestamp}_${branch}.jar" -T $jar_file
