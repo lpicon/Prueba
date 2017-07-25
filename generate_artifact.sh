c122ad503a8db8441816135b2fa64fa04d03f524
picon <mvn-release>
2017-07-25 12:32:17 -0500

    version 1

--- generate_artifact.sh
+++ generate_artifact.sh
@@ -2,4 +2,4 @@ timestamp=`date +%Y%m%d%H%M%S`
 branch=`git branch | grep \* | cut -d ' ' -f2`
 jar_file=`ls target/*.jar | head -n 1`
 
-curl -X PUT "https://bancodebogota.jfrog.io/bancodebogota/repojuli/${timestamp}_${branch}.jar" -T $jar_file
+curl -X PUT "https://bancodebogota.jfrog.io/bancodebogota/qa/CustomerBlackListInquiryAdapter/${timestamp}_${branch}.jar" -T $jar_file
